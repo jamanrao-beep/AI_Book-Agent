5804686afc33a9f74e196cd721d9514ce56d6a89
Aman J <jamanrao@gmail.com>
2026-06-12 19:59:56 +0530

    changed the def port

--- book-agent/backend/start.py
+++ book-agent/backend/start.py
@@ -2,6 +2,6 @@ import os
 # pyrefly: ignore [missing-import]
 import uvicorn
 
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 8080))
 print(f"Starting on port {port}")
 uvicorn.run("main:app", host="0.0.0.0", port=port)
